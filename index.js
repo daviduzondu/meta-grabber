067b7578c8f7e8c8f3ed83fffe216e5f1eb9937a
daviduzondu <uzondudavid2004@gmail.com>
2023-08-05 09:21:42 +0100

    Made a few changes

--- index.js
+++ index.js
@@ -3,35 +3,41 @@ import  cheerio  from "cheerio";
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
+import fs from "fs";
+import moment from "moment";
 
 const app = express();
 app.use(cors())
 
 let html;
-
-let metadata = {img:"", description:"", error:false, message:""};
+let metadata = {img:"", description:"", error:true, message:""};
 
 async function getMainPage(target, res){
-    metadata.img="";
-    metadata.description="";
-    metadata.error=false;
-    metadata.message="";
     try {
-            const response = await fetch(target)
+        const response = await fetch(target)
             html = await response.text();
-    
             const $ = cheerio.load(html);
-            $('meta').each(function(){
-                if($(this).attr('property')==="og:image") metadata.img = $(this)['0'].attribs.content;
-                if($(this).attr('property')==="og:description") metadata.description = $(this)['0'].attribs.content;
+            $('meta').each(function(index){
+                if($(this).attr('property')==="og:image" && !metadata.img) {metadata.img = $(this)['0'].attribs.content; metadata.error=false; metadata.message+="image_"}
+
+                if($(this).attr('name')?.includes("image") && !metadata.img) {metadata.img = $(this)['0'].attribs.content; metadata.error=false; metadata.message+="image_"}
+
+                if($(this).attr('property')==="og:description" && !metadata.description){ metadata.description = $(this)['0'].attribs.content; metadata.error=false; metadata.message+="description_"}
+
+                if($(this).attr('name')?.includes("description") && !metadata.description){ metadata.description = $(this)['0'].attribs.content; metadata.error=false; metadata.message="description_"}
+
+                if(index===$('meta').length - 1 && !metadata.img && !metadata.description) {throw new Error('not_found')}
             })    
             
         } catch (error) {
             metadata.error=true;
             metadata.message=error.message;
+        } finally {
+            res.json(metadata);
+            metadata = {img:"", description:"", error:true, message:"not_found"};
         }
-        res.json(metadata);
 }
+
 
 app.get('/', (req, res)=>{
     if(req.query.url) getMainPage(JSON.parse(req.query.url), res);
@@ -41,6 +47,7 @@ app.get('/', (req, res)=>{
 app.get('*', (req, res)=>{
     getMainPage(null, res);
 })
+
 
 
 app.listen(process.env.PORT || PORT)
