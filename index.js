74752846942ab4417c891424dc49afc68390b794
daviduzondu <uzondudavid2004@gmail.com>
2023-08-05 09:47:28 +0100

    Made a few changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import  cheerio  from "cheerio";
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
+import url from 'url';
 import fs from "fs";
 import moment from "moment";
 
@@ -13,18 +14,28 @@ let html;
 let metadata = {img:"", description:"", error:true, message:""};
 
 async function getMainPage(target, res){
+    let options = url.parse(target);
     try {
         const response = await fetch(target)
             html = await response.text();
             const $ = cheerio.load(html);
-            $('meta').each(function(index){
-                if($(this).attr('property')==="og:image" && !metadata.img) {metadata.img = $(this)['0'].attribs.content; metadata.error=false; metadata.message+="image_"}
 
-                if($(this).attr('name')?.includes("image") && !metadata.img) {metadata.img = $(this)['0'].attribs.content; metadata.error=false; metadata.message+="image_"}
+            function formatURL(link){
+                if (!link.startsWith("http")){
+                    return options.protocol + "//" + options.host + link;
+                } else {
+                    return link
+                }
+            }
+
+            $('meta').each(function(index){
+                if($(this).attr('property')==="og:image" && !metadata.img) {metadata.img = formatURL($(this)['0'].attribs.content); metadata.error=false; metadata.message+="image_"}
+
+                if($(this).attr('name') === "image" && !metadata.img) {metadata.img = formatURL($(this)['0'].attribs.content); metadata.error=false; metadata.message+="imagde_"}
 
                 if($(this).attr('property')==="og:description" && !metadata.description){ metadata.description = $(this)['0'].attribs.content; metadata.error=false; metadata.message+="description_"}
 
-                if($(this).attr('name')?.includes("description") && !metadata.description){ metadata.description = $(this)['0'].attribs.content; metadata.error=false; metadata.message="description_"}
+                if($(this).attr('name') === "description" && !metadata.description){ metadata.description = $(this)['0'].attribs.content; metadata.error=false; metadata.message="description_"}
 
                 if(index===$('meta').length - 1 && !metadata.img && !metadata.description) {throw new Error('not_found')}
             })    
